e1f917bbdd4884050703823407d6aa2f345c1009
maayan <maayanbah@gmail.com>
2025-02-11 22:25:45 +0200

    Integrate Mongoose with genres API

--- index.js
+++ index.js
@@ -1,72 +1,20 @@
-const Joi = require("joi");
 const express = require("express");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/VidLib")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(() => console.error("Could not connect to MongoDB"));
 
 app.use(express.json());
-
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-}
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 app.get("/", (req, res) => {
   res.send("VidLib");
-});
-
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.filter((id) => id === parseInt(req.params.id));
-
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-  res.send(genre);
-});
-
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.put("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
 });
 
 const port = process.env.PORT || 3000;
---
+++ routes/genres.js
@@ -0,0 +1,72 @@
+const Joi = require("joi");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: { type: String, required: true, minlength: 5, maxlength: 50 },
+  })
+);
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(genre);
+}
+
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
+});
+
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  res.send(genre);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let genre = new Genre({
+    name: req.body.name,
+  });
+
+  genre = await genre.save();
+  res.send(genre);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  res.send(genre);
+});
+
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndDelete(req.params.id);
+
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  res.send(genre);
+});
+
+module.exports = router;
