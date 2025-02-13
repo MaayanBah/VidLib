fcce3849c97e7c5836788ebb3b56ba67dcc71a95
maayan <maayanbah@gmail.com>
2025-02-13 17:33:31 +0200

    Add ID validation to the movies API and refactor

--- models/movie.js
+++ models/movie.js
@@ -33,7 +33,7 @@ const Movie = mongoose.model(
 function validateMovie(movie) {
   const schema = Joi.object({
     title: Joi.string().max(50).required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number(),
     dailyRentalRate: Joi.number(),
   });
