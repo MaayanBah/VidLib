c249d3f3a2ed09043b9e3b556ea6ee703ca92833
maayan <maayanbah@gmail.com>
2025-02-15 15:48:07 +0200

    Implement a basic users API

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 1024,
+    },
+  })
+);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
